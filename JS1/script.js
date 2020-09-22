49805e0d9f8b3ca2d9fe3b18988492c323423b14
Mike <mikhail-kazantsev-2020@mail.ru>
2020-09-22 12:37:48 +0300

    функции практика

--- JS1/script.js
+++ JS1/script.js
@@ -1,24 +1,3 @@
 /* jshint -W097 */
 "use strict";
 
-console.log(4 + '5');
-
-let incr = 10,
-    decr = 10;
-
-// incr++;
-// decr--;
-
-console.log(++incr);
-console.log(--decr);
-
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 != 8);
-
-
-const isChecked = false,
-    isClose = true;
-
-console.log(isChecked || !isClose);
